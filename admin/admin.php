5ec0ef6d9d0f3b959e9ffd41a758578d5977520f
jumriani21 <jumriani521@gmail.com>
2022-01-14 15:14:45 +0800

    perbuahan administrasi

--- admin/admin.php
+++ admin/admin.php
@@ -17,7 +17,7 @@
 <div class="container-fluid">
   <div class="row">
     <div class="col-xs-2" id="sideLeft">
-      <h4><strong>Administrator</strong></h4>
+      <h4><strong>administrasi</strong></h4>
       <ul class="nav nav-pills nav-stacked" id="data">
         <li class="active"><a data-toggle="tab" href="#user">User</a></li>
         <li><a data-toggle="tab" href="#barang">Barang</a></li>
